6e4f064ebc57c029394682f19e897c0e536cf3bc
dov <benscheinberg@gmail.com>
2025-07-30 15:07:42 -0400

    fixed server rendering

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/server.ts'],
   outDir: 'dist',
   tsconfig: './tsconfig.build.json', // Use build-specific tsconfig
   sourcemap: true,
